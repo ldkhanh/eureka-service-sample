a3dc98d37e7b5cd65d5900290a0384f276428af4
khanhduyle <khanhduyle@assembla.com>
2018-05-09 18:38:49 -0700

    first commit

---
+++ hello-client/src/main/java/khanh/cloud/helloclient/HelloClientApplication.java
@@ -0,0 +1,12 @@
+package khanh.cloud.helloclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloClientApplication.class, args);
+	}
+}
